0df0c7aad459e722599b48e7775963eda5186e80
Mahesh <42913060+vmahesh17@users.noreply.github.com>
2024-04-05 12:32:20 +0530

    Mv test sample lambda

---
+++ playwright.config.ts
@@ -0,0 +1,77 @@
+import type { PlaywrightTestConfig } from "@playwright/test";
+import { devices } from "@playwright/test";
+
+// Playwright config to run tests on LambdaTest platform and local
+const config: PlaywrightTestConfig = {
+  testDir: "tests",
+  // testMatch: ["tests\loginTest.spec.ts"],
+  timeout: 60000,
+  use: {},
+  projects: [
+    // -- LambdaTest Config --
+    // name in the format: browserName:browserVersion:platform@lambdatest
+    // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+    // Use additional configuration options provided by Playwright if required: https://playwright.dev/docs/api/class-testconfig
+    {
+      name: "chrome:latest:MacOS Ventura@lambdatest",
+      use: {
+        viewport: { width: 1920, height: 1080 },
+      },
+    },
+    // {
+    //   name: "chrome:latest:Windows 11@lambdatest",
+    //   use: {
+    //     viewport: { width: 1280, height: 720 },
+    //   },
+    // },
+    // {
+    //   name: "MicrosoftEdge:latest:MacOS Ventura@lambdatest",
+    //   use: {
+    //     ...devices["iPhone 12 Pro Max"],
+    //   },
+    // },
+    // {
+    //   name: "pw-firefox:latest:Windows 11@lambdatest",
+    //   use: {
+    //     viewport: { width: 1280, height: 720 },
+    //   },
+    // },
+    // {
+    //   name: "pw-webkit:latest:Windows 10@lambdatest",
+    //   use: {
+    //     viewport: { width: 1920, height: 1080 },
+    //   },
+    // },
+    // Config for running tests in local
+    // {
+    //   name: "chrome",
+    //   use: {
+    //     browserName: "chromium",
+    //     channel: "chrome",
+    //   },
+    // },
+    // {
+    //   name: "safari",
+    //   use: {
+    //     browserName: "webkit",
+    //     viewport: { width: 1200, height: 750 },
+    //   },
+    // },
+    // {
+    //   name: "firefox",
+    //   use: {
+    //     browserName: "firefox",
+    //     viewport: { width: 800, height: 600 },
+    //   },
+    // },
+    // // Test in mobile viewport.
+    // {
+    //   name: "chrome@pixel5",
+    //   use: {
+    //     ...devices['iPhone 12 Pro Max'],
+    //   }
+    // },
+  ],
+};
+
+export default config;
---
+++ tests/loginTest1.spec.ts
@@ -0,0 +1,38 @@
+import { beforeEach } from "node:test";
+ import { expect,test  } from "@playwright/test";
+
+const data ={
+    URL : "https://ecommerce-playground.lambdatest.io/",
+    USERNAME: "Vemula.Mahesh@coforge.com",
+    PASSWORD:"M@hi1712"
+}
+
+test.describe('E commerce ', async()=>{
+    test.beforeEach(async( {page})=>{
+        test.step('Launch Application ', async()=>{
+            await page.goto(data.URL);
+            await page.waitForLoadState('networkidle');
+        })
+       
+    })
+
+    test('Login to application', async({page})=>{
+        await page.waitForTimeout(8000)
+        await page.hover('//*[@data-toggle="dropdown"]//span[contains(text(),"My account")]');
+        await page.getByRole('link', { name: 'Login' }).click();
+
+        const userNameTxt = '#input-email';
+        const passwordTxt='#input-password';
+        await page.waitForLoadState('domcontentloaded')
+
+        await page.fill(userNameTxt,data.USERNAME);
+        await page.fill(passwordTxt,data.PASSWORD);
+        await page.getByRole('button', { name: 'Login' }).click()
+        await page.waitForTimeout(8000)
+
+
+        expect(page.getByText('Account', { exact: true }))
+    })
+
+
+})
